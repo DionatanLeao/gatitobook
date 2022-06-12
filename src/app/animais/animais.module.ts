dd46ab74cc0030bf4c442785ca69878609aeeba0
DionatanLeao <dionatan.ti@outlook.com>
2022-06-12 15:01:35 -0300

    Componente de detalhe

--- src/app/animais/animais.module.ts
+++ src/app/animais/animais.module.ts
@@ -7,13 +7,15 @@ import { AnimalComponent } from './animal/animal.component';
 import { CartaoComponent } from '../componentes/cartao/cartao.component';
 import { CartaoModule } from '../componentes/cartao/cartao.module';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 
 
 @NgModule({
   declarations: [
     ListaAnimaisComponent,
     AnimalComponent,
-    GradeFotosAnimaisComponent
+    GradeFotosAnimaisComponent,
+    DetalheAnimalComponent
   ],
   imports: [
     CommonModule,
