d1ba73d7b8b1daeafcf6cfe2602fdc97b5846ced
DionatanLeao <dionatan.ti@outlook.com>
2022-06-13 11:23:27 -0300

    Métodos de curtir e excluir foto

--- src/app/animais/animais.service.ts
+++ src/app/animais/animais.service.ts
@@ -1,27 +1,37 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, mapTo, Observable, of, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { TokenService } from '../autenticacao/token.service';
 import { Animais, Animal } from './animais';
 
 const API = environment.apiUrl
+const NOT_MODIFIED = '304'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AnimaisService {
 
-  constructor(
-    private httpClient: HttpClient,
-    private tokenService: TokenService) { }
+  constructor(private http: HttpClient) { }
 
   listaUsuario(nomeUsuario: string) : Observable<Animais> {
-    return this.httpClient.get<Animais>(`${API}/${nomeUsuario}/photos`)
+    return this.http.get<Animais>(`${API}/${nomeUsuario}/photos`)
   }
 
   buscaPorId(id:number): Observable<Animal> {
-    return this.httpClient.get<Animal>(`${API}/photos/${id}`)
+    return this.http.get<Animal>(`${API}/photos/${id}`)
   } 
+
+  excluiAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`) 
+  }
+
+  curtir(id: number): Observable<boolean> {
+    return this.http
+      .post(`${API}/photos/${id}/likes`, {}, {observe: 'response'})
+      .pipe(mapTo(true), catchError((error) => {
+        return error.status === NOT_MODIFIED ? of(false) : throwError(error)
+      }))
+  }
 
 }
