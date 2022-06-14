62bd7248a4a9733a9640c3fa1b5d31e7f899ed71
DionatanLeao <dionatan.ti@outlook.com>
2022-06-14 14:13:09 -0300

    Correção da API

--- src/app/animais/animais-routing.module.ts
+++ src/app/animais/animais-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { ListaAnimaisResolver } from './lista-animais/lista-animais.resolver';
@@ -10,21 +10,21 @@ const routes: Routes = [
     path: '',
     component: ListaAnimaisComponent,
     resolve: {
-      animais: ListaAnimaisResolver
-    }
-  },
-  {
-    path: ':animalId',
-    component: DetalheAnimalComponent
+      animais: ListaAnimaisResolver,
+    },
   },
   {
     path: 'novo',
-    component: NovoAnimalComponent
-  }
+    component: NovoAnimalComponent,
+  },
+  {
+    path: ':animalId',
+    component: DetalheAnimalComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AnimaisRoutingModule { }
+export class AnimaisRoutingModule {}
